5caf3a7824a02490321dcce4d62e4ac79009c149
lizzy <lizzyaustad@gmail.com>
2015-11-28 21:19:38 -0500

    Updating files.

--- jquery.js
+++ jquery.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
+    
     $(".circle1").one("click",function(){
-        $("#2").animate({left: '200px',height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-        $("#3").animate({right: '200px',height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-        $("#4").animate({right: '50px', bottom: '+=5',height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-        $("#5").animate({right: '100px', bottom: '-=175', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-        $("#6").animate({right: '50px',bottom: '-=175', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-        $("#7").animate({left: '200px',bottom: '+=175', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
-document.getElementById("resumelogo").style.visibility='visible';  
-document.getElementById("projectlogo").style.visibility='visible';        document.getElementById("fblogo").style.visibility='visible';
-    document.getElementById("githublogo").style.visibility='visible';
-        document.getElementById("linkedinlogo").style.visibility='visible';
-document.getElementById("email").style.visibility='visible';
-        document.getElementById("goback").style.visibility='visible';
+        if ($.browser.mozilla) {
+            $("#2").animate({position: "relative",left: '175px', bottom: '-=25', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#3").animate({position: "relative",right: '175px', bottom: '-=25', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#4").animate({position: "relative",right: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#5").animate({position: "relative",right: '175px', bottom: '-=625', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#6").animate({position: "relative",left: '175px',bottom: '-=625', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#7").animate({position: "relative",left: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        }
+        else {
+        $("#2").animate({position: "relative",left: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#3").animate({position: "relative",right: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#4").animate({position: "relative",right: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#5").animate({position: "relative",right: '175px', bottom: '-=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#6").animate({position: "relative",left: '175px',bottom: '-=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        $("#7").animate({position: "relative",left: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
+        }
+        
+document.getElementById("2").style.visibility='visible';  
+document.getElementById("3").style.visibility='visible'; 
+        document.getElementById("4").style.visibility='visible';
+    document.getElementById("5").style.visibility='visible';
+        document.getElementById("6").style.visibility='visible';
+document.getElementById("7").style.visibility='visible';
+        
     }); 
+        
     
     $("#2").click(function(){
         window.open("Resume.pdf");
