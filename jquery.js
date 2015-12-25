873970a18a365d7a3e11b1d10d177b4bb840166d
lizzy <lizzyaustad@gmail.com>
2015-12-24 20:25:41 -0600

    Updating files.

--- jquery.js
+++ jquery.js
@@ -1,32 +1,40 @@
 $(document).ready(function(){
-    
-    //$(".circle1").one("click",function(){
-        if ($.browser.mozilla) {
-            $("#2").animate({position: "relative",left: '175px', bottom: '-=25', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},"slow");
+
+    if ($.browser.mozilla) {
+        $("#2").animate({position: "relative",left: '175px', bottom: '-=25', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},"slow");
         $("#3").animate({position: "relative",right: '175px', bottom: '-=25', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
         $("#4").animate({position: "relative",right: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
         $("#5").animate({position: "relative",right: '175px', bottom: '-=625', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
         $("#6").animate({position: "relative",left: '175px',bottom: '-=625', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
         $("#7").animate({position: "relative",left: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'});
         }
-        else {
-        $("#2").animate({position: "relative",left: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},750);
-        $("#3").animate({position: "relative",right: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},500);
-        $("#4").animate({position: "relative",right: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},1750);
-        $("#5").animate({position: "relative",right: '175px', bottom: '-=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},1500);
-        $("#6").animate({position: "relative",left: '175px',bottom: '-=300', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},1250);
-        $("#7").animate({position: "relative",left: '350px', height: '50px', width: '50px', padding: '50px', fontSize: '1.5em'},1000);
+    else {
+        $("#2").animate({position: "relative",left: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},750);
+        $("#3").animate({position: "relative",right: '175px', bottom: '+=300', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},500);
+        $("#4").animate({position: "relative",right: '350px', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},1750);
+        $("#5").animate({position: "relative",right: '175px', bottom: '-=300', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},1500);
+        $("#6").animate({position: "relative",left: '175px',bottom: '-=300', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},1250);
+        $("#7").animate({position: "relative",left: '350px', height: '50px', width: '50px', padding: '40px', fontSize: '1.5em'},1000);
         }
         
-document.getElementById("2").style.visibility='visible';  
-document.getElementById("3").style.visibility='visible'; 
-        document.getElementById("4").style.visibility='visible';
+    document.getElementById("2").style.visibility='visible';  
+    document.getElementById("3").style.visibility='visible'; 
+    document.getElementById("4").style.visibility='visible';
     document.getElementById("5").style.visibility='visible';
-        document.getElementById("6").style.visibility='visible';
-document.getElementById("7").style.visibility='visible';
+    document.getElementById("6").style.visibility='visible';
+    document.getElementById("7").style.visibility='visible';
         
     //}); 
-        
+      $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+           $(window).scroll(function() {
+                var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+               var coords = '50% '+ yPos + 'px';
+                
+                // Move the background
+                $bgobj.css({ backgroundPosition: coords });
+            });
+        });   
     
     $("#resumelogo").click(function(){
         window.open("Resume.pdf");
